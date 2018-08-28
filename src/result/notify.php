6d9fe33e8c2ec218c78be7b9b969943832ed6e74
Markus Hobisch <markus.hobisch@wirecard.com>
2018-08-28 08:37:45 +0200

    changes

--- src/result/notify.php
+++ src/result/notify.php
@@ -29,6 +29,7 @@ require_once('../util/helperFunctions.php');
 
 // To include the necessary files, we use the composer for PSR-4 autoloading.
 require '../../vendor/autoload.php';
+require '../config.php';
 
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
@@ -45,23 +46,29 @@ $config = new Config\Config($baseUrl, $httpUser, $httpPass, 'EUR');
 // ### Validation
 
 // We use Monolog as logger. Set up a logger for the notifications.
-$log = new Logger('Wirecard notifications');
+$logger = new Logger('Wirecard notifications');
 try {
-    $log->pushHandler(new StreamHandler('../logs/notify.log', Logger::INFO));
+    $logger->pushHandler(new StreamHandler('../logs/notify.log', Logger::INFO));
 } catch (Exception $e) {
-    $log->info("Exception thrown in StreamHandler.");
+    $logger->info("Exception thrown in StreamHandler.");
 }
+
+echo "notify.php --> define logger";
 
 // Set a public key for certificate pinning used for response signature validation, this certificate needs to be always
 // up to date
 $config->setPublicKey(file_get_contents('../../certificate/api-test.wirecard.com.crt'));
+
+echo "notify.php --> set public key";
 
 // ## Transaction
 
 // ### Transaction Service
 
 // The `TransactionService` is used to determine the response from the service provider.
-$service = new TransactionService($config);
+$service = new TransactionService($config, $logger);
+
+echo "notify.php --> set transaction service";
 
 // ## Notification status
 
@@ -69,15 +76,17 @@ $service = new TransactionService($config);
 $notification = $service->handleNotification(file_get_contents('php://input'));
 // Log the notification for a successful transaction.
 
+echo "notify.php --> handle notification";
+
 if ($notification instanceof SuccessResponse) {
-    $log->info(sprintf(
+    $logger->info(sprintf(
         'Transaction with id %s was successful and validation status is %s.',
         $notification->getTransactionId(),
         $notification->isValidSignature() ? 'true' : 'false'
     ));
 // Log the notification for a failed transaction.
 } elseif ($notification instanceof FailureResponse) {
-    $log->info(sprintf(
+    $logger->info(sprintf(
         'Transaction with id %s was failure and validation status is %s.',
         $notification->getTransactionId(),
         $notification->isValidSignature() ? 'true' : 'false'
@@ -92,6 +101,6 @@ if ($notification instanceof SuccessResponse) {
         $severity = ucfirst($status->getSeverity());
         $code = $status->getCode();
         $description = $status->getDescription();
-        $log->warning(sprintf('%s with code %s and message "%s" occurred.<br>', $severity, $code, $description));
+        $logger->warning(sprintf('%s with code %s and message "%s" occurred.<br>', $severity, $code, $description));
     }
 }
